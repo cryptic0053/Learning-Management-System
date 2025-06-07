e88f25a5b2e688cff8bd98a727d3b47ff80b148b
cryptic0053 <anirbanargha0053@gmail.com>
2025-06-07 14:18:50 +0600

    frontend-start

--- lms_frontend/src/App.jsx
+++ lms_frontend/src/App.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Profile from './pages/Profile';
+import './App.css'; 
 
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <BrowserRouter>
+      <div>Navbar</div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+        <div>Footer</div>
+      </BrowserRouter>
     </div>
   )
 }
