dc7f9b03be1526e1b7fa91b044779771cc1ddaab
cryptic0053 <anirbanargha0053@gmail.com>
2025-06-15 22:06:18 +0600

    done

--- lms_frontend/src/App.jsx
+++ lms_frontend/src/App.jsx
@@ -1,26 +1,37 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Profile from "./pages/Profile";
-import "./App.css";
-import Navbar from "./components/header/Navbar";
+import { Route, Routes } from "react-router";
+import MainLayout from "./layout/MainLayout";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import CoursesPage from "./pages/CoursesPage";
+import StudentDashboard from "./pages/StudentDashboard";
+import { CourseDetailsPage } from "./pages/CourseDetailsPage";
+import { DashboardLayout } from "./layout/DashboardLayout";
 
 function App() {
   return (
-    <div>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/profile" element={<Profile />} />
-        </Routes>
-        <div>Footer</div>
-      </BrowserRouter>
-    </div>
+    <Routes>
+      <Route element={<MainLayout />}>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/courses" element={<CoursesPage />} />
+        <Route path="/courses/:courseId" element={<CourseDetailsPage />} />
+
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+
+        <Route
+          path="/teacher/dashboard"
+          element={<div>PROTECTED Dashboard</div>}
+        />
+
+        <Route path="/student/dashboard" element={<StudentDashboard />} />
+
+        <Route path="*" element={<div>404</div>} />
+      </Route>
+      {/* <Route element={<DashboardLayout/>}>
+        <Route path="/student/dashboard" element={<StudentDashboard />} />
+      </Route> */}
+    </Routes>
   );
 }
 
